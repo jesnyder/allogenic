7a62271705502c0f91983ee356b36d27450342ef
jesnyder <jesyndre@gmail.com>
2021-12-14 12:48:50 -0800

    updated

--- code/python/c0000_main.py
+++ code/python/c0000_main.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 from c0100_chart_clinical import chart_clinical
 from c0200_chart_patents import chart_patents
+from c0300_chart_trials import chart_trials
 
 
 def main():
@@ -13,6 +14,11 @@ def main():
 
     1. Chart allogenic vs autolous clinical trials
 
+    Before running the code
+    Navigate to the following url
+    https://clinicaltrials.gov/ct2/search/advanced?cond=&term=&cntry=&state=&city=&dist=
+    Search terms and save in data/source folder
+ 
     """
 
     print("running main")
@@ -20,11 +26,11 @@ def main():
     # Review the note above
     # Identify which tasks need to be run
     # List the task numbers that need to be run below
-    tasks = [1, 2]
+    tasks = [3]
 
     if 1 in tasks: chart_clinical()
     if 2 in tasks: chart_patents()
-
+    if 3 in tasks: chart_trials()
 
 
     print("completed main")
